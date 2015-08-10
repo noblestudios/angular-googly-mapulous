15ea6ecacef83e158b40cb5f42a9891e74dd2834
Tim Shaw <tim.shaw@noblestudios.com>
2015-08-10 10:27:58 -0700

    Updated directive to allow manual-init attr to be passed in to account for the directive being nested (it will be initted before the parent controller is)

--- dist/googly-mapulous.js
+++ dist/googly-mapulous.js
@@ -20,20 +20,10 @@ angular.module( 'googlyMapulous' ).directive( 'googleMap', [ 'googleMaps', funct
   return {
     restrict: 'E',
     template: '<div class="google-map-container"><div class="google-map"></div></div>',
-    controller: [ '$scope', '$element', '$compile', function ( $scope, $element, $compile ) {
-      // Set basic map config up
-      // These can be overridden by config options contained in
-      // $scope.mapConfig
-      var options = {};
-      if ( $scope.mapConfig && $scope.mapConfig instanceof Object ) {
-        Object.keys( $scope.mapConfig ).forEach( function ( key ) {
-          options[ key ] = $scope.mapConfig[ key ];
-        });
-      }
-
+    controller: [ '$scope', '$element', '$compile', '$attrs', function ( $scope, $element, $compile, $attrs ) {
       // Check for presence of manual init var in $scope.  Wait for manual call
       // before initting if set, otherwise create the map immediately
-      if ( $scope.mapManualInit ) {
+      if ( $scope.mapManualInit || typeof( $attrs.manualInit ) !== 'undefined' ) {
         var unsubscribe = $scope.$on( 'initializeGoogleMap', function () {
           initMap();
 
@@ -52,6 +42,16 @@ angular.module( 'googlyMapulous' ).directive( 'googleMap', [ 'googleMaps', funct
        * when finished.
        */
       function initMap () {
+        // Set basic map config up
+        // These can be overridden by config options contained in
+        // $scope.mapConfig
+        var options = {};
+        if ( $scope.mapConfig && $scope.mapConfig instanceof Object ) {
+          Object.keys( $scope.mapConfig ).forEach( function ( key ) {
+            options[ key ] = $scope.mapConfig[ key ];
+          });
+        }
+
         // Build the map and save a reference to the created map object in the
         // $scope for reference later from controller
         $scope.googleMap = new googleMaps.GoogleMap( $element[ 0 ].children[ 0 ].children[ 0 ], $scope, $compile, options );
